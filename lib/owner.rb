c0d5e19851c4ff249b6b7218d8629eff338bdbfe
CleanSolLLC <mamark90@gmail.com>
2019-09-21 10:41:59 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_accessor
+
+  @@all = []  
+
+  def initialize(species = "human", name)
+    @species = species
+    @@all << self
+    @name = name
+  end 
+
+  def name
+    @name
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+
+
+
+
+
+
 end
+
+
+
